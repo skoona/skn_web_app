1ad6f49c731b7d5b31ed41f8e707c6c1a87ca99a
James Scott Jr <skoona@gmail.com>
2018-10-15 01:17:54 -0400

    Updating basic config for database.

--- config/version.rb
+++ config/version.rb
@@ -1,9 +1,9 @@
 # A better way to say it
 module Skn
   class Version
-    MAJOR = 0
-    MINOR = 4
-    PATCH = 2
+    MAJOR = 1
+    MINOR = 0
+    PATCH = 0
 
     def self.to_s
       [MAJOR, MINOR, PATCH].join('.')
