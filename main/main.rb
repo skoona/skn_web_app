ad7236d7b0d23fe9c880027df168f34c7974f3e7
James Scott <JScott@brotherhoodmutual.com>
2018-10-17 11:24:09 -0400

    Refactoring with Configurable and Registry complete.

--- main/main.rb
+++ main/main.rb
@@ -8,11 +8,3 @@ require_relative 'utils/utils'
 require_relative 'persistence/persistence'
 require_relative 'authentication/authentication'
 require_relative 'services/services'
-
-# ##
-# Configure our command processors
-SknApp.registry
-  .register(:catalog_provider, ->(command) { Services::Providers::Catalog.call(command) }, call: false)
-  .register(:content_provider, ->(command) { Services::Providers::Content.call(command) }, call: false)
-  .register(:file_handler, ->(response) { Services::Handlers::File.call(response) }, call: false)
-  .register(:get_handler, ->(uri) { Services::Handlers::GetRequest.call(uri) }, call: false)
--- main/services/services.rb
+++ main/services/services.rb
@@ -4,13 +4,20 @@
 #  Local Exceptions
 # ##
 
+class CommandFailedValidation < StandardError
+end
+
+# ##
+# Configure our command processors
+SknApp.registry
+    .register(:catalog_provider, ->(command) { Services::Providers::Catalog.call(command) }, call: false)
+    .register(:content_provider, ->(command) { Services::Providers::Content.call(command) }, call: false)
+    .register(:file_handler, ->(response) { Services::Handlers::File.call(response) }, call: false)
+    .register(:get_handler, ->(uri) { Services::Handlers::GetRequest.call(uri) }, call: false)
+
 # ##
 # Each Directory has a same named file that handles it's includes
 # ##
-
-# Represents Rest request failure of an
-class CommandFailedValidation < StandardError
-end
 
 require_relative 'commands/commands'
 require_relative 'handlers/handlers'
