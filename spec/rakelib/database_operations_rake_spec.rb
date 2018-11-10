9e000c8a7e5cc74a263a9a49c86872306e4ef593
James Scott Jr <skoona@gmail.com>
2018-11-09 22:05:04 -0500

    Updating Docs.

--- spec/rakelib/database_operations_rake_spec.rb
+++ spec/rakelib/database_operations_rake_spec.rb
@@ -6,14 +6,11 @@ describe "rake db:seed", type: :task do
     expect(task.prerequisites).not_to include "environment"
   end
 
-  # it "runs workers comp path" do
-  #   allow(Quoting::Request).to receive_message_chain(:new, :work_comp_template).and_return(restore_test_data('brms_template_data'))
-  #   allow(WorkCompQuestionMap).to receive(:create).and_return(true)
+  # it "runs db:seed rake" do
   #
   #   r = task.execute
   #
   #   expect(r).to be_an Array
-  #   expect(r).to_not be_empty
   # end
 end
 
@@ -24,9 +21,7 @@ describe "rake db:skn_setup", type: :task do
     expect(task.prerequisites).not_to include "environment"
   end
 
-  # it "runs work_comp path" do
-  #   allow(Quoting::GatherWorkCompQuestions).to receive_message_chain(:call).and_return(restore_test_data('rules_gathered_questions'))
-  #   allow(Quoting::GatherCmpQuestions).to receive_message_chain(:call).and_return(restore_test_data('rules_gathered_questions'))
+  # it "runs db:skn_setup rake" do
   #
   #   expect{ task.invoke(BmiTypes::Lob::WORK_COMP) }.to(
   #       output( match(/Writing WORK_COMP CSV.../) ).to_stdout )
